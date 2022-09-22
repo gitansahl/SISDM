b712601e90db009eddd21d6ff9e3ab7975ae9562
Gitan Sahl Wijaya <gitan.sahl@ui.ac.id>
2022-09-22 19:23:35 +0700

    init

---
+++ src/main/java/apap/tugas1/tugas1_SISDM_2006596522/Tugas1Sisdm2006596522Application.java
@@ -0,0 +1,13 @@
+package apap.tugas1.tugas1_SISDM_2006596522;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tugas1Sisdm2006596522Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tugas1Sisdm2006596522Application.class, args);
+	}
+
+}
---
+++ src/test/java/apap/tugas1/tugas1_SISDM_2006596522/Tugas1Sisdm2006596522ApplicationTests.java
@@ -0,0 +1,13 @@
+package apap.tugas1.tugas1_SISDM_2006596522;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tugas1Sisdm2006596522ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
